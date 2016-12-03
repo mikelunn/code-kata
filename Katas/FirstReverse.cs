ef21267dcfd6859eb0c05d8a1fe694ce69fb9a11
Mike Lunn <mlunn01@gmail.com>
2016-12-03 11:34:38 -0500

    Added Challenge

--- Katas/FirstReverse.cs
+++ Katas/FirstReverse.cs
@@ -6,6 +6,9 @@ using System.Threading.Tasks;
 
 namespace Katas
 {
+    /// <summary>
+    /// Using the C# language, have the function FirstReverse(str) take the str parameter being passed and return the string in reversed order. For example: if the input string is "Hello World and Coders" then your program should return the string sredoC dna dlroW olleH. 
+    /// </summary>
     public static class FirstReverse
     {
         public static string Execute(string str)
