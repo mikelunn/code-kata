518d048fa16933036e87d2fbfa743c9226c0a5cd
Mike Lunn <mlunn01@gmail.com>
2016-12-03 11:33:07 -0500

    Added FirstReverse

---
+++ Kata.Tests/FirstReverseTest.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Katas;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Kata.Tests
+{
+    [TestClass]
+    public class FirstReverseTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Assert.AreEqual("etybredoc", FirstReverse.Execute("coderbyte"));
+            Assert.AreEqual("edoC evoL I", FirstReverse.Execute("I Love Code"));
+        }
+    }
+}
---
+++ Katas/FirstReverse.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Katas
+{
+    public static class FirstReverse
+    {
+        public static string Execute(string str)
+        {
+            var arr = str.ToCharArray();
+            Array.Reverse(arr);
+            return new string(arr);
+
+        }
+    }
+}
