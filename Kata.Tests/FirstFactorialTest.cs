a9daf71fc8818f8c1be23deb6485d92ec7cd56b1
Mike Lunn <mlunn01@gmail.com>
2016-12-03 09:34:44 -0500

    Added First Factorial with recursive solution

---
+++ Kata.Tests/FirstFactorialTest.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Katas;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Kata.Tests
+{
+    /// <summary>
+    /// Using the C# language, have the function
+    ///  FirstFactorial(num) take the num parameter
+    ///  being passed and return the factorial
+    ///  of it (e.g. if num = 4, return (4 * 3 * 2 * 1)).
+    ///  For the test cases, the range will be 
+    /// between 1 and 18 and the input will always be an integer. 
+    /// </summary>
+    [TestClass]
+    public class FirstFactorialTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Assert.AreEqual(24, FirstFactorial.Execute(4));
+            Assert.AreEqual(40320, FirstFactorial.Execute(8));
+        }
+    }
+}
---
+++ Katas/FirstFactorial.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Katas
+{
+    public static class FirstFactorial
+    {
+        public static int Execute(int num)
+        {
+            if (num == 0)
+            {
+                return 1;
+            }
+            return num*Execute(num - 1);
+        }
+    }
+}
