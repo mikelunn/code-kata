b7adedaa9e9f452a95af985a64a3a7eb48e62e9e
Mike Lunn <mlunn01@gmail.com>
2016-12-04 10:00:24 -0500

    Added Longest Word

---
+++ Kata.Tests/LongestWordTest.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Katas;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Kata.Tests
+{
+    /// <summary>
+    /// Using the C# language, have the function LongestWord(sen) take the sen parameter being passed and return the largest word in the string. If there are two or more words that are the same length, return the first word from the string with that length. Ignore punctuation and assume sen will not be empty. 
+    /// </summary>
+    [TestClass]
+    public class LongestWordTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Assert.AreEqual("time", LongestWord.Execute("fun&!! time"));
+            Assert.AreEqual("love", LongestWord.Execute("I love dogs"));
+          
+        }
+    }
+}
---
+++ Katas/LongestWord.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Katas
+{
+    public static class LongestWord
+    {
+        public static string Execute(string sen)
+        {
+            return Regex.Replace(sen, "[^0-9a-zA-Z]+", " ")
+                .Split(' ')
+                .OrderByDescending(c => c.Length)
+                .FirstOrDefault();
+        }
+    }
+}
